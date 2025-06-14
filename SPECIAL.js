c06f5e532e97991f38f8da2e75725e58105c0b97
Ethan <68720253+ethannp@users.noreply.github.com>
2025-06-14 00:41:20 -0400

    Update SPECIAL.js

--- SPECIAL.js
+++ SPECIAL.js
@@ -1,3 +1,7 @@
-const recommendedpuzzles = [{author: "intro_puzzle_bot", emoji: "🤖", puzzles: ['1234', '4211', '1111', '6072', '0612', '5095', '3004', '8432']}, {author: "fluff", emoji: "⭐", puzzles: ['0003', '0004', '0300', '1254', '2253', '2551', '3451', '3565', '2542', '3332', '4367', '4658', '4851', '5246', '7585', '8235', '8456']}, {author: "phenomist", emoji: "🌁", puzzles: ['9598', '9958', '3495', '8656', '2868', '7537', '7556', '4898', '4394', '8951', '5694', '4955', '3955', '6869', '8519']}, {author: "abiteofdata", emoji: "🦎", puzzles: ['7800']}, {author: "n3rl", emoji: "🦐", puzzles: ['9387']}]
+const recommendedpuzzles = [{author: "intro_puzzle_bot", emoji: "🤖", puzzles: ['1234', '4211', '1111', '6072', '0612', '5095', '3004', '8432']}, 
+    {author: "fluff", emoji: "⭐", puzzles: ['0003', '0004', '0300', '1254', '2253', '2551', '3451', '3565', '2542', '3332', '4367', '4658', '4851', '5246', '7585', '8235', '8456']}, 
+    {author: "phenomist", emoji: "🌁", puzzles: ['9598', '9958', '3495', '8656', '2868', '7537', '7556', '4898', '4394', '8951', '5694', '4955', '3955', '6869', '8519']}, 
+    {author: "abiteofdata", emoji: "🦎", puzzles: ['1196', '7800']}, 
+    {author: "n3rl", emoji: "🦐", puzzles: ['9387']}]
 const challenge = [{puz: '1254', restrictions: '+-'}, {puz: '3332', restrictions: '+-'}, {puz: '8235', restrictions: '+-'}, {puz: '8432', restrictions: '+-'}, {puz: '3495', restrictions: '+-'}, {puz: '9598', restrictions: '+-'}, {puz: '7585', restrictions: '-'}, {puz: '8951', restrictions: '!'}]
 const DEFAULT_SETTINGS = "00000000" // unsolved, completed, recommended, random, has-challenge, includerandom, multisolve, openinsandbox
