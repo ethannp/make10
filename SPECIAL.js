5e3214793d18443d4f6679641db8d5a90c584160
Ethan <68720253+ethannp@users.noreply.github.com>
2025-06-15 20:04:24 -0400

    Update SPECIAL.js

--- SPECIAL.js
+++ SPECIAL.js
@@ -3,6 +3,6 @@ const recommendedpuzzles = [{author: "intro_puzzle_bot", emoji: "🤖", puzzles:
     {author: "phenomist", emoji: "🌁", puzzles: ['9598', '9958', '3495', '8656', '2868', '7537', '7556', '4898', '4394', '8951', '5694', '4955', '3955', '6869', '8519']}, 
     {author: "abiteofdata", emoji: "🦎", puzzles: ['1196', '7800', '7333', '5257']}, 
     {author: "n3rl", emoji: "🦐", puzzles: ['9387']},
-    {author: "elara", emoji: "⛰️", puzzles: ['5998']}]
+    {author: "elara", emoji: "⛰️", puzzles: ['5998', '4908']}]
 const challenge = [{puz: '1254', restrictions: '+-'}, {puz: '3332', restrictions: '+-'}, {puz: '8235', restrictions: '+-'}, {puz: '8432', restrictions: '+-'}, {puz: '3495', restrictions: '+-'}, {puz: '9598', restrictions: '+-'}, {puz: '7585', restrictions: '-'}, {puz: '8951', restrictions: '!'}, {puz: '5998', restrictions: '/'}, {puz: '7333', restrictions: '+-'}]
 const DEFAULT_SETTINGS = "00000000" // unsolved, completed, recommended, random, has-challenge, includerandom, multisolve, openinsandbox
