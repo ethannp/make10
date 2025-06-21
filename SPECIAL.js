4321922c5ee8fbe3b7b8d1d9a83c233d29e58950
Ethan <68720253+ethannp@users.noreply.github.com>
2025-06-21 16:19:33 -0400

    Update SPECIAL.js

--- SPECIAL.js
+++ SPECIAL.js
@@ -1,7 +1,7 @@
 const recommendedpuzzles = [{author: "intro_puzzle_bot", emoji: "🤖", puzzles: ['1234', '4211', '1111', '6072', '0612', '5095', '3004', '8432']}, 
     {author: "fluff", emoji: "⭐", puzzles: ['0003', '0004', '0300', '1254', '2253', '2551', '3451', '3565', '2542', '3332', '4367', '4658', '4851', '5246', '7585', '8235', '8456']}, 
     {author: "phenomist", emoji: "🌁", puzzles: ['9598', '9958', '3495', '8656', '2868', '7537', '7556', '4898', '4394', '8951', '5694', '4955', '3955', '6869', '8519']}, 
-    {author: "abiteofdata", emoji: "🦎", puzzles: ['1196', '7800', '7333', '5257']}, 
+    {author: "abiteofdata", emoji: "🦎", puzzles: ['1196', '7800', '7333', '5257', '7588']}, 
     {author: "n3rl", emoji: "🦐", puzzles: ['9387']},
     {author: "elara", emoji: "⛰️", puzzles: ['5998', '4908']}]
 const challenge = [{puz: '1254', restrictions: '+-'}, 
